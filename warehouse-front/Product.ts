1e70d40156b7cc1344afa872424d9b8bfe719b9e
Steven Ngo <ngoitzsteven@gmail.com>
2022-07-26 23:29:46 -0400

    second

--- warehouse-front/Product.ts
+++ warehouse-front/Product.ts
@@ -2,12 +2,15 @@ export class Product {
     id: number;
     productName: String;
     partNumber: String;
-
+    supply: number;
+    warehouseId: number
     
-    constructor( id = 0, productName = '', partNumber = ''){
+    constructor( id = 0, productName = '', partNumber = '', supply = 0, warehouseId = 0){
         this.id = id;
         this.productName = productName;
         this.partNumber = partNumber;
+        this.supply = supply;
+        this.warehouseId = warehouseId;
     }
 
 
